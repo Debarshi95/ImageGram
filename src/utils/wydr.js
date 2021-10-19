82ca4521a9af628adfd6419e24be01d15b378118
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2021-10-19 11:24:18 +0530

    Minor fix, disabled test run

--- src/utils/wydr.js
+++ src/utils/wydr.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 if (process.env.NODE_ENV === 'development') {
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
-  console.log('UEE');
+
   whyDidYouRender(React, {
     trackAllPureComponents: true,
   });
